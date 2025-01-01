7317176f06783fd1b7f714932454a040684be8d5
Helal-AIUB <154461509+Helal-AIUB@users.noreply.github.com>
2025-01-01 23:27:11 +0600

    Add files via upload

---
+++ Week - 09/Day - 01/Problem-03.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define endl '\n'
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define ll long long int
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    ll n,a,b,p,q;
+    cin>>n>>a>>b>>p>>q;
+    ll cnt1=0,cnt2=0,cnt3=0;
+    for(int i=1; i<=n; i++){
+        if(i%a==0){
+            cnt1++;
+        }
+        if(i%b==0){
+            cnt2++;
+        }
+        if((i%a==0) && (i%b==0)){
+            cnt3++;
+        }
+    }
+    cnt1=cnt1-cnt3;
+    cout<<(cnt1*p)+(cnt2*q);
+    return 0;
+}
