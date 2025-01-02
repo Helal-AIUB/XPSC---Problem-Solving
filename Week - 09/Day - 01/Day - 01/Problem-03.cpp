f969073e7722e8f1b9deadc0e9420f1ff15f6660
Helal-AIUB <154461509+Helal-AIUB@users.noreply.github.com>
2025-01-02 13:47:54 +0600

    Update Problem-03.cpp
    updated

--- Week - 09/Day - 01/Day - 01/Problem-03.cpp
+++ Week - 09/Day - 01/Day - 01/Problem-03.cpp
@@ -5,26 +5,32 @@
 #define no cout<<"NO"<<endl
 #define ll long long int
 using namespace std;
-
+ll LCM(ll a,ll b){
+    return (a/__gcd(a,b))*b;
+}
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     ll n,a,b,p,q;
     cin>>n>>a>>b>>p>>q;
-    ll cnt1=0,cnt2=0,cnt3=0;
-    for(int i=1; i<=n; i++){
-        if(i%a==0){
-            cnt1++;
-        }
-        if(i%b==0){
-            cnt2++;
-        }
-        if((i%a==0) && (i%b==0)){
-            cnt3++;
-        }
-    }
-    cnt1=cnt1-cnt3;
-    cout<<(cnt1*p)+(cnt2*q);
+    ll x=(n/a)*p, y=(n/b)*q, overlap=(n/LCM(a,b));
+    ll ans=((x+y)-(overlap*(p+q)))+(overlap*max(p,q));
+    cout<<ans<<endl;
+    
+    // ll cnt1=0,cnt2=0,cnt3=0;
+    // for(int i=1; i<=n; i++){
+    //     if(i%a==0){
+    //         cnt1++;
+    //     }
+    //     if(i%b==0){
+    //         cnt2++;
+    //     }
+    //     if((i%a==0) && (i%b==0)){
+    //         cnt3++;
+    //     }
+    // }
+    // cnt1=cnt1-cnt3;
+    // cout<<(cnt1*p)+(cnt2*q);
     return 0;
 }
