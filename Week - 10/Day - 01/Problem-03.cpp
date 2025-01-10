e8c59f8c21f2d06f262e82d72f82bc9c5ddc4ece
Helal-AIUB <154461509+Helal-AIUB@users.noreply.github.com>
2025-01-10 22:57:17 +0600

    Add files via upload

---
+++ Week - 10/Day - 01/Problem-03.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#define all(x) x.begin(),x.end()
+#define endl '\n'
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define ll long long int
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n,q;
+    cin>>n>>q;
+    vector<int> a(n);
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+    }
+    for(int i=0; i<q; i++){
+        int key;
+        cin>>key;
+        int l=0,r=n-1,mid,ans=-1;
+        while(l<=r){
+            mid=(l+r)/2;
+            if(key <= a[mid]){
+                ans=mid;
+                r=mid-1;
+            }else{
+                l=mid+1;
+            }
+        }
+        //cout<<(ans+1)<<endl;
+        if((ans+1)==0){
+            cout<<n+1<<endl;
+        }else{
+            cout<<ans+1<<endl;
+        }
+    }
+    return 0;
+}
