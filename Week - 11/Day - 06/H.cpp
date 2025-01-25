c47f530d7f503f9b3fdbafb5c6b8cb6f756bc9e9
Helal-AIUB <154461509+Helal-AIUB@users.noreply.github.com>
2025-01-25 23:51:58 +0600

    Update H.cpp

--- Week - 11/Day - 06/H.cpp
+++ Week - 11/Day - 06/H.cpp
@@ -1,46 +1,46 @@
 #include<bits/stdc++.h>
-#define all(x) x.begin(),x.end()
-#define endl '\n'
-#define yes cout<<"YES"<<endl
-#define no cout<<"NO"<<endl
 #define ll long long int
 using namespace std;
 
-int main()
-{
-    ios_base::sync_with_stdio(0);
+int main() {
+    ios_base::sync_with_stdio(false);
     cin.tie(0);
-    ll n;
-    cin>>n;
+
+    int n;
+    cin >> n;
     vector<ll> a(n);
-    ll even=0,odd=0;
-    for(ll i=0; i<n; i++){
-        cin>>a[i];
-        if(a[i]%2==0){
-            even++;
-        }else{
-            odd++;
+    vector<ll> odd, even;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        if (a[i] % 2 == 0)
+            even.push_back(a[i]);
+        else
+            odd.push_back(a[i]);
+    }
+
+    sort(odd.begin(), odd.end());
+    sort(even.begin(), even.end());
+
+    int odd_size = odd.size(), even_size = even.size();
+    if (abs(odd_size - even_size) <= 1) {
+        cout << 0 << endl;
+        return 0;
+    }
+
+    ll sum = 0;
+    if (odd_size > even_size) {
+        int excess = odd_size - (even_size + 1);
+        for (int i = 0; i < excess; i++) {
+            sum += odd[i];
+        }
+    } 
+    else {
+        int excess = even_size - (odd_size + 1);
+        for (int i = 0; i < excess; i++) {
+            sum += even[i];
         }
     }
-    if((even==odd-1) || (odd==even-1)){
-         cout<<0<<endl;
-    }else{
-         sort(a.begin(),a.end());
-         if(even>odd){
-            ll x=even-(odd+1);
-            ll sum=0;
-            for(ll i=0; i<x; i++){
-                sum +=a[i];
-            }
-            cout<<sum<<endl;
-         }else{
-            ll x=odd-(even+1);
-            ll sum=0;
-            for(ll i=0; i<x; i++){
-                sum +=a[i];
-            }
-            cout<<sum<<endl;
-         }
-    }
+
+    cout << sum << endl;
     return 0;
 }
